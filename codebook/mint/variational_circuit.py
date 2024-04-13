6c7e2a5dadfc28da55d196baa14be87072c589b7
Shisheer S Kaushik <76246051+ShisheerKaushik24@users.noreply.github.com>
2024-04-13 15:14:05 +0530

    Add files via upload

---
+++ codebook/mint/variational_circuit.py
@@ -0,0 +1,25 @@
+# the write_and_run function writes the content in this cell into the file "variational_circuit.py"
+
+### WRITE YOUR CODE BETWEEN THESE LINES - START
+    
+# import libraries that are used in the function below.
+from qiskit import QuantumCircuit
+from qiskit.circuit import ParameterVector
+from qiskit.circuit.library import  RealAmplitudes, EfficientSU2, TwoLocal, NLocal
+    
+### WRITE YOUR CODE BETWEEN THESE LINES - END
+
+def variational_circuit(num_qubits):
+    # BUILD VARIATIONAL CIRCUIT HERE - START
+    
+    # import required qiskit libraries if additional libraries are required
+    
+    # build the variational circuit
+    var_circuit = EfficientSU2(num_qubits, su2_gates=["rx", "y"], entanglement="linear", reps=3, insert_barriers=True) #EfficientSU2(num_qubits, entanglement='linear', reps=2, insert_barriers=True)
+
+    #var_circuit = EfficientSU2(num_qubits, entanglement='linear', reps=2, insert_barriers=True)
+    #var_circuit = TwoLocal(num_qubits, ['ry', 'rz'], ['cx'], entanglement='linear', reps=4, insert_barriers=True)
+    # BUILD VARIATIONAL CIRCUIT HERE - END
+    
+    # return the variational circuit which is either a VaritionalForm or QuantumCircuit object
+    return var_circuit, num_qubits
